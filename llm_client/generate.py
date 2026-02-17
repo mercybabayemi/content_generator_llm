dc45675422587863eb46aeec7175f2a87788bbf2
mercybabayemi <mercyjanet013@gmail.com>
2026-02-17 12:03:01 +0000

    Added generator llm client, fast api deployment version and notebook

---
+++ llm_client/generate.py
@@ -0,0 +1,19 @@
+
+from transformers import AutoModelForCausalLM, AutoTokenizer
+import torch
+
+model_name = "mistralai/Mistral-7B-v0.1"
+
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+model = AutoModelForCausalLM.from_pretrained(
+    model_name,
+    device_map="auto",
+    dtype=torch.float16
+)
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+def generate(prompt, max_new_tokens=200):
+    inputs = tokenizer(prompt, return_tensors="pt").to(device)
+    outputs = model.generate(**inputs, max_new_tokens=max_new_tokens)
+    return tokenizer.decode(outputs[0], skip_special_tokens=True)
