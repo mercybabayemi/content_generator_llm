554ea119b577947dfe7fb552b33e34eded60bd9b
Gloria Oyewusi <gloriaoyewusii@gmail.com>
2026-03-11 14:27:28 +0100

    Program agent schema all done.

--- all_agents_folders/ProgramAgent/schema.py
+++ all_agents_folders/ProgramAgent/schema.py
@@ -1,1 +1,12 @@
+from pydantic import BaseModel
+from core.enums import ComplexityLevel
 
+class ProgramAgentSchema(BaseModel):
+    program_name: str
+    description: str
+    complexity_level: ComplexityLevel
+    learning_outcomes: str
+    objectives: str
+    prerequisites: str
+    introductory_video: str|None
+    image: str|None
